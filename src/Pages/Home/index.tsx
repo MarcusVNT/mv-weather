4141025b601d99decafd02590745d2ea84cf355a
Marcus Vinícius <marcus-vnt@live.com>
2024-09-10 17:18:20 -0300

    Change on HomePage

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,23 +1,11 @@
-import { Stack, Typography } from '@mui/material'
-import Grid from '@mui/material/Grid2'
-import PeriodNav from './PeriodNav'
+import Hero from './Hero'
+// import PeriodNav from './PeriodNav'
 
 export default function HomePage() {
   return (
-    <Stack
-      alignItems="center"
-      m="40px 16px"
-      boxShadow="rgba(184, 160, 140, 0.5) 0px 19px 38px, rgba(214, 205, 188, 0.5) 0px 15px 12px;
-      "
-    >
-      <Grid border="1px solid #000" width="100%" bgcolor="#f9f4f0">
-        <PeriodNav />
-        <Grid p="8px">
-          <Typography variant="h1" component="h1">
-            MV Weather
-          </Typography>
-        </Grid>
-      </Grid>
-    </Stack>
+    <>
+      <PeriodNav />
+      {/* <Hero /> */}
+    </>
   )
 }
